c045142bb41cc3e814a3dcd0d7fb9c09fbdced4b
aljimenez97 <aljimenez97@gmail.com>
2022-03-10 14:26:23 -0800

    lowest common ancestor

--- binary_tree/medium/lower_common_ancestor.py
+++ binary_tree/medium/lower_common_ancestor.py
@@ -9,10 +9,30 @@
 # to be a descendant of itself).”
 
 class Node:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, val) -> None:
+        self.val = val
+        self.right = None
+        self.left = None
+
 def lowest_common_ancestor(tree_node, p, q):
-    return
+
+    def dfs(node):
+        if node is None: 
+            return False
+
+        current = node == p or node == q
+
+        left = dfs(node.left)
+        right = dfs(node.right)
+
+        if (left and right) or (current and left) or (current and right):
+            return node
+
+        return current or left or right
+
+    return dfs(tree_node)
+
+
 if __name__ == "__main__":
     print(lowest_common_ancestor())
 
