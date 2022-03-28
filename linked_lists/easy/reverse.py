7265ad5666b8230d54944dbd56b6b483e9bd57a2
aljimenez97 <aljimenez97@gmail.com>
2022-03-28 11:15:08 -0700

    reverse linked list II

--- linked_lists/easy/reverse.py
+++ linked_lists/easy/reverse.py
@@ -6,6 +6,27 @@ class Node:
     def __str__(self) -> str:
         return str(self.val) + " " + str(self.next) if self.next is not None else str(self.val)
 
+def reverseBetween(self, head, left: int, right: int):
+    
+    dummy = Node(0)
+    dummy.next = head
+    prevLeft, curr = dummy, dummy.next
+    
+    for i in range(left - 1):
+        prevLeft, curr = curr, curr.next
+        
+    prev = None
+    
+    for i in range(right - left + 1):
+        tmpAux = curr.next
+        curr.next = prev
+        prev, curr = curr, tmpAux
+        
+    prevLeft.next.next = curr
+    prevLeft.next = prev
+    
+    return dummy.next
+
 def reverseList(head):
     prev = None
     curr = head
